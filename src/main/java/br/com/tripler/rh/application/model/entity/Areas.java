8ecbddea6c8be2422045de4b51ad787a69782509
Fernando Freitag <fmfreitag01@gmail.com>
2025-09-11 18:08:18 -0300

    Ajustes para conectar com Mysql

--- src/main/java/br/com/tripler/rh/application/model/entity/Areas.java
+++ src/main/java/br/com/tripler/rh/application/model/entity/Areas.java
@@ -1,14 +1,55 @@
 package br.com.tripler.rh.application.model.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import org.jspecify.annotations.NonNull;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "areas", schema="hr")
 public class Areas {
     @Id
-    @NonNull
-    private Float areaId;
-    private String areaNome;
-    private Float areaDeptoId;
-    private Float areaEncFuncId;
+    @Column(name="id")
+    private Long areaId;
 
+    @Column(name="area_nome")
+    private String areaNome;
+
+    @Column(name="area_depto_id")
+    private Long areaDeptoId;
+
+    @Column(name="area_enc_func_id")
+    private Long areaEncFuncId;
+
+    public Long getAreaId() {
+        return areaId;
+    }
+
+    public void setAreaId(Long areaId) {
+        this.areaId = areaId;
+    }
+
+    public String getAreaNome() {
+        return areaNome;
+    }
+
+    public void setAreaNome(String areaNome) {
+        this.areaNome = areaNome;
+    }
+
+    public Long getAreaDeptoId() {
+        return areaDeptoId;
+    }
+
+    public void setAreaDeptoId(Long areaDeptoId) {
+        this.areaDeptoId = areaDeptoId;
+    }
+
+    public Long getAreaEncFuncId() {
+        return areaEncFuncId;
+    }
+
+    public void setAreaEncFuncId(Long areaEncFuncId) {
+        this.areaEncFuncId = areaEncFuncId;
+    }
 }
--- src/main/java/br/com/tripler/rh/application/repository/SetorRepository.java
+++ src/main/java/br/com/tripler/rh/application/repository/SetorRepository.java
@@ -1,4 +1,8 @@
 package br.com.tripler.rh.application.repository;
 
-public class SetorRepository {
+import br.com.tripler.rh.application.model.entity.Setor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SetorRepository extends JpaRepository<Setor, Long> {
+
 }
